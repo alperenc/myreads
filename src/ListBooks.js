978449f28d41601f30718bf823d04e782a58f604
Alp Eren Can <alperenc@gmail.com>
2017-10-27 16:46:19 +0300

    add default books and the Bookshelf component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
+import startCase from 'lodash.startcase'
+import Bookshelf from './Bookshelf'
 
 class ListBooks extends Component {
   render() {
     return (
       <div className="list-books">
         <div className="list-books-title">
-          <h1>MyReads</h1>
+          <h1>{this.props.title}</h1>
         </div>
         <div className="list-books-content">
           <div>
-            <div className="bookshelf">
+            {this.props.books.map(book => book.shelf).filter((value, index, self) => self.indexOf(value) === index).map(shelf => (
+              <Bookshelf key={shelf} title={startCase(shelf)} books={this.props.books.filter(book => book.shelf === shelf)} />
+            ))}
+            {/* <div className="bookshelf">
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
@@ -155,7 +160,7 @@ class ListBooks extends Component {
                   </li>
                 </ol>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="open-search">
