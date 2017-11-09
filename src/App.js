3c1ad86890cdb938b11428d582568f7ee2c4013b
Alp Eren Can <alperenc@gmail.com>
2017-11-09 18:29:04 +0300

    Use Route components to control page content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import SearchBooks from './components/SearchBooks'
 import ListBooks from './components/ListBooks'
@@ -6,14 +7,7 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    books: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   }
 
   changeShelf = (book, shelf) => {
@@ -29,26 +23,22 @@ class BooksApp extends React.Component {
     BooksAPI.update(book, shelf)
   }
 
-  displaySearchPage = (event) => {
-    this.setState(state => ({
-      showSearchPage: event
-    }))
-  }
-
   componentDidMount() {
     BooksAPI.getAll().then(books => this.setState({ books }))
   }
 
   render() {
-    const { books, showSearchPage } = this.state
+    const { books } = this.state
 
     return (
       <div className="app">
-        {showSearchPage ? (
-          <SearchBooks onChangeShelf={this.changeShelf} onDisplaySearchPage={this.displaySearchPage} />
-        ) : (
-            <ListBooks title="MyReads" books={books} onChangeShelf={this.changeShelf} onDisplaySearchPage={this.displaySearchPage} />
-          )}
+        <Route exact path="/" render={() => (
+          <ListBooks title="MyReads" books={books} onChangeShelf={this.changeShelf} />
+        )} />
+
+        <Route path="/search" render={() => (
+          <SearchBooks onChangeShelf={this.changeShelf} />
+        )} />
       </div>
     )
   }
