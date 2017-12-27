305e628160092f16f547207ef78bdec180d6f92a
Alp Eren Can <alperenc@gmail.com>
2017-12-27 18:14:35 +0300

    Refactor Book to be a stateless functional component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-class Book extends Component {
-  render() {
-    const { book, onChangeShelf } = this.props
-
-    return (
-      <div className="book" >
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
-          <div className="book-shelf-changer">
-            <select value={book.shelf ? book.shelf : "none"} onChange={(event) => onChangeShelf(book, event.target.value)}>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
-      </div >
-    )
-  }
-}
+const Book = ({ book, onChangeShelf }) => (
+  <div className="book" >
+    <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
+      <div className="book-shelf-changer">
+        <select value={book.shelf ? book.shelf : "none"} onChange={(event) => onChangeShelf(book, event.target.value)}>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    </div>
+    <div className="book-title">{book.title}</div>
+    <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
+  </div >
+)
 
 Book.propTypes = {
   onChangeShelf: PropTypes.func.isRequired,
