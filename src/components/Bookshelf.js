c0293e6e25fb33c91b39efeadb8752dc9572d01a
Alp Eren Can <alperenc@gmail.com>
2017-12-27 18:11:22 +0300

    Make Bookshelf cleaner

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -2,24 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
-const Bookshelf = (props) => {
-  const { title, books, onChangeShelf } = props
-
-  return (
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">{title}</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {books.map(book => (
-            <li key={book.id}>
-              <Book book={book} onChangeShelf={onChangeShelf} />
-            </li>
-          ))}
-        </ol>
-      </div>
+const Bookshelf = ({ title, books, onChangeShelf }) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books.map(book => (
+          <li key={book.id}>
+            <Book book={book} onChangeShelf={onChangeShelf} />
+          </li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  </div>
+)
 
 Bookshelf.propTypes = {
   title: PropTypes.string.isRequired,
